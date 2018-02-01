1b100534ed75aaa7ed426e04e98547e8bfa72a6e
evilosa <bojkov@sorc.ru>
2018-02-01 16:00:27 +0300

    Refactored project structure.

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,21 @@
 import Action from './Action';
-
+import ActionsPane from './ActionsPane';
 import Button from './Button';
+import ComponentsLayout from './ComponentsLayout';
+import ControlledForm from './ControlledForm';
 import NavBar from './NavBar';
 import NavBarItem from './NavBarItem';
+import TextInput from './TextInput';
+import UpdateBlocker from './UpdateBlocker';
 
-export { Action, Button, NavBar, NavBarItem };
+export {
+  Action,
+  ActionsPane,
+  Button,
+  ComponentsLayout,
+  ControlledForm,
+  NavBar,
+  NavBarItem,
+  TextInput,
+  UpdateBlocker,
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import * as components from './components';
 import containers from './containers';
 import models from './models';
+import * as theme from './theme';
 
-export { components, containers, models };
-export default { components,  containers, models };
+export { components, containers, models, theme };
+export default { components,  containers, models, theme };
